dfbcaa8cdf0d5694ed2485ce815bb3134b75c661
Andreas Larsson <Andreas.per.larsson@gmail.com>
2022-04-27 16:49:40 +0200

    first commit for form

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Form test!</h1>
+<SignUpForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignUpForm from './components/Signupform.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {SignUpForm}
 }
 </script>
+
 
 <style>
 #app {
@@ -22,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+margin:0;
+background: #eee;
 }
 </style>
---
+++ src/components/Signupform.vue
@@ -0,0 +1,72 @@
+<template>
+  <form>
+    <label>Email</label>
+    <input type="email" required v-model="email">
+    
+    <label>Password</label>
+    <input type="password" required v-model="password">
+    
+    <label>Role</label>
+    <select v-model="role">
+    <option value="developer">Webdeveloper</option>
+    <option value="agent">Agent</option>
+    </select>
+
+<div class="terms">
+    <input v-model="checkbox" type="checkbox" required>
+    <label>Accept terms and conditions</label>
+</div>
+
+  </form>
+  <p>Email : {{email}}</p>
+  <p>Password : {{password}}</p>
+  <p>Roll : {{role}}</p>
+  <p>Check : {{checkbox}}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: '',
+            checkbox: false,
+        }
+    }
+}
+</script>
+
+<style>
+form{
+    max-width: 400px;
+    margin: 50px auto;
+    display: flex;
+    align-items: left;
+    flex-direction: column;
+    background-color:white;
+    padding: 30px;
+    border-radius: 10px;
+    border: 1px solid black
+}
+input{
+    width: 100%;
+    font-size: 20px;
+}
+label{
+    text-align: left;
+    font-size: 18px;
+    margin-bottom: 10px;
+    opacity: 80;
+}
+
+.terms{
+    display: flex;
+    justify-content: start;
+    margin-top: 10px;
+}
+.terms input{
+    padding: 0;
+    width: 20px;
+}
+</style>
